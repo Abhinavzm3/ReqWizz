0ce3517683b393e6c739f9b7b14ca6d701e4bb0f
Abhinavzm3 <abhinavzm99@gmail.com>
2026-07-05 23:18:53 +0530

    feat: db connection established

---
+++ server/db.js
@@ -0,0 +1,22 @@
+// dbConnect.js
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined in .env");
+    }
+
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`✔️  MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌  MongoDB connection error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
